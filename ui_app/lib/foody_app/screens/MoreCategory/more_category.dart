e952a87803f66f6adc770e9a3045b7cab2c6784d
trantrihung <hung01638482021@gmail.com>
2023-04-24 17:42:34 +0700

    start task

---
+++ ui_app/lib/foody_app/screens/MoreCategory/more_category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MoreCategory extends StatelessWidget {
+  const MoreCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
